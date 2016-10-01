44e0971db7acbd7c43055f3f1836352c7607b5da
Christoph Zamaitat <czamaitat@gmail.com>
2016-10-01 21:26:15 +0200

    adds songs_controller code

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,29 @@
 class SongsController < ApplicationController
-    def new
 
+    def index
+        @song = Song.all
     end
+
+    def new
+        @song = Song.new
+    end
+
+    def create
+        @song = Song.new(song_params)
+
+        if @song.save
+            redirect_to @song
+        else
+            render 'new'
+        end
+    end
+
+    def show
+        @song = Song.find(params[:id])
+    end
+
+    private
+        def song_params
+            params.require(:song).permit(:title, :musician, :originalkey)
+        end
 end
