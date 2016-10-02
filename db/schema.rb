08cb5c498fe1f381bdc2e0837d4e34dc61b9ac1f
Christoph Zamaitat <czamaitat@gmail.com>
2016-10-02 22:19:18 +0200

    new database schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161001174024) do
+ActiveRecord::Schema.define(version: 20161002194645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,4 +24,28 @@ ActiveRecord::Schema.define(version: 20161001174024) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "transcribers", force: :cascade do |t|
+    t.string   "name"
+    t.text     "instruments"
+    t.string   "email"
+    t.string   "facebook_link"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  create_table "transcriptions", force: :cascade do |t|
+    t.integer "song_id"
+    t.integer "transcriber_id"
+    t.string  "level"
+    t.string  "key"
+    t.text    "lyrics"
+    t.string  "video"
+    t.text    "comment"
+  end
+
+  add_index "transcriptions", ["song_id"], name: "index_transcriptions_on_song_id", using: :btree
+  add_index "transcriptions", ["transcriber_id"], name: "index_transcriptions_on_transcriber_id", using: :btree
+
+  add_foreign_key "transcriptions", "songs"
+  add_foreign_key "transcriptions", "transcribers"
 end
