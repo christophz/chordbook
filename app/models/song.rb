538402a5b7c1d973d2543d28762ac5bb89af0a7d
Christoph Zamaitat <czamaitat@gmail.com>
2016-10-02 22:18:24 +0200

    adds model code to SONG

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,6 @@
 class Song < ActiveRecord::Base
+    has_many :transcriptions
+    
+    validates :originalkey, :length => { :maximum => 2 }
+    validates :title, :originalkey, :musician, :presence => true
 end
