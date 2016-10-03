ee928740b798c243d6f4eb3902dc21e78cb3857c
Christoph Zamaitat <czamaitat@gmail.com>
2016-10-03 11:59:52 +0200

    doesn't need URL validation anymore

--- app/models/transcription.rb
+++ app/models/transcription.rb
@@ -5,6 +5,5 @@ class Transcription < ActiveRecord::Base
     validates :level, :key, :lyrics, :presence => true
     validates :comment, :length => { :maximum => 100 }
     validates :lyrics, :length => { :maximum => 10000 }
-    validates_format_of :video, :with => URI::regexp(%w(http https))
 
 end
