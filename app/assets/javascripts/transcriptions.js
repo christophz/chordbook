7cd3a054615dc8edec5e66ff206e01a058ad1b01
Christoph Zamaitat <czamaitat@gmail.com>
2016-10-05 16:41:12 +0200

    basis function for transposing chords

--- app/assets/javascripts/transcriptions.js
+++ app/assets/javascripts/transcriptions.js
@@ -1,2 +1,12 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+function transposeChord(chord, amount) {
+    var scale = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+
+    return chord.replace(/[CDEFGAB]#?/g,
+        function(match) {
+            var i = (scale.indexOf(match) + amount) % scale.length;
+            return scale[ i < 0 ? i + scale.length : i ];
+        });
+}
+
+console.log(transposeChord("Dm7/G", 2)); // gives "Em7/A"
+console.log(transposeChord("Fmaj9#11", -23)); // gives "F#maj9#11"
