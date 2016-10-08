ec628422f659b82c863c0b09cc20760360c9fee2
Christoph Zamaitat <czamaitat@gmail.com>
2016-10-08 21:04:53 +0200

    adds DEVISE config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'chordbook-htw.herokuapp.com', port: 443 }
 end
