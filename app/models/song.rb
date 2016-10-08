53e02f3e84699c73b228ece112645c7d3c71cd5e
Christoph Zamaitat <czamaitat@gmail.com>
2016-10-08 17:36:06 +0200

    enable cascading deletion

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,6 @@
 class Song < ActiveRecord::Base
-    has_many :transcriptions
-    
+    has_many :transcriptions, :dependent => :delete_all
+
     validates :originalkey, :length => { :maximum => 2 }
     validates :title, :originalkey, :musician, :presence => true
 end
