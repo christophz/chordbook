12ea008120bc0d9a9f3e3a5ddd00eda1a957cc66
Christoph Zamaitat <czamaitat@gmail.com>
2016-10-08 21:05:27 +0200

    adds belongs_to association

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,6 @@
 class Song < ActiveRecord::Base
     has_many :transcriptions, :dependent => :delete_all
+    belongs_to :transcriber
 
     validates :originalkey, :length => { :maximum => 2 }
     validates :title, :originalkey, :musician, :presence => true
