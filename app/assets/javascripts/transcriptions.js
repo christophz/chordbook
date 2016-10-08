0d26652775b31d260a8b420ae6f13d0d8aa2f63c
Christoph Zamaitat <czamaitat@gmail.com>
2016-10-08 17:31:29 +0200

    adjusts Transpose script

--- app/assets/javascripts/transcriptions.js
+++ app/assets/javascripts/transcriptions.js
@@ -1,12 +1,47 @@
-function transposeChord(chord, amount) {
-    var scale = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+$(document).ready(function () {
 
-    return chord.replace(/[CDEFGAB]#?/g,
-        function(match) {
-            var i = (scale.indexOf(match) + amount) % scale.length;
-            return scale[ i < 0 ? i + scale.length : i ];
+    var match;
+    var chords =
+        ['C','C#','D','Eb','E','F','F#','G','Ab','A','Bb','B','C',
+         'Db','D','D#','E','F','Gb','G','G#','A','A#','C'];
+    var chordRegex = /C#|D#|F#|G#|A#|Db|Eb|Gb|Ab|Bb|C|D|E|F|G|A|B/g;
+
+    function transpose(that, direction) {
+        var currentChord = that.text();
+        var output = "";
+        var parts = currentChord.split(chordRegex);
+        var index = 0;
+        while (match = chordRegex.exec(currentChord))
+        {
+            if (direction == "up") {
+                var chordIndex = chords.indexOf(match[0]);
+                output += parts[index++] + chords[chordIndex+1];
+            } else if (direction == "down") {
+                var chordIndex = chords.indexOf(match[0],1);
+                output += parts[index++] + chords[chordIndex-1];
+            }
+        }
+        output += parts[index];
+        that.text(output);
+    }
+
+    $('#transposeUp').click(function() {
+        $('#transcription em').each(function() {
+            transpose($(this), "up");
         });
-}
 
-console.log(transposeChord("Dm7/G", 2)); // gives "Em7/A"
-console.log(transposeChord("Fmaj9#11", -23)); // gives "F#maj9#11"
+        $('.transcription__key').each(function() {
+            transpose($(this), "up");
+        });
+    });
+
+    $('#transposeDown').click(function() {
+        $('#transcription em').each(function() {
+            transpose($(this), "down");
+        });
+
+        $('.transcription__key').each(function() {
+            transpose($(this), "down");
+        });
+    });
+});
