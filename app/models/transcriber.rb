3581f2257b617c299668a794e08da1f6dbdf66b4
Christoph Zamaitat <czamaitat@gmail.com>
2016-10-08 21:04:19 +0200

    new transcriber properties

--- app/models/transcriber.rb
+++ app/models/transcriber.rb
@@ -1,8 +1,10 @@
 class Transcriber < ActiveRecord::Base
-    has_many :transcriptions
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 
-    validates :name, :instruments, :email, :presence => true
-    validates :instruments, :length => { :maximum => 100 }
-    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  has_many :transcriptions
+  has_many :songs
 
 end
